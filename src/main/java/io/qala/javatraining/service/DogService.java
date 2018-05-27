5dbf1682039a0d510e52eaf0831f7d098954518f
timazet <14309920+timazet@users.noreply.github.com>
2018-05-27 21:17:08 +0300

    Puzzle was solved

--- src/main/java/io/qala/javatraining/service/DogService.java
+++ src/main/java/io/qala/javatraining/service/DogService.java
@@ -6,24 +6,36 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
+/**
+ * There is a problem with final methods.
+ * Due to the fact that we don't have here any aspectj weaving and proxy-target-class=true attribute is set
+ * for <tx:annotation/> element, we implicitly use CGLIB to create proxies with transactional logic.
+ * But as we know there is no place for final methods, because they can't be overridden during extending.
+ * And we face with a problem when CGLIB forced to create an instance of DogService using constructor but can't wrap methods
+ * marked with transactional annotation due to final methods.
+ */
 public class DogService {
     public DogService(DogDao dogDao) {
         this.dogDao = dogDao;
     }
 
-    @Transactional public final Collection<Dog> getAllDogs() {
+    @Transactional
+    public Collection<Dog> getAllDogs() {
         return dogDao.getAllDogs();
     }
 
-    @Transactional public final Dog getDog(String id) {
+    @Transactional
+    public Dog getDog(String id) {
         return dogDao.getDog(id);
     }
 
-    @Transactional public final Dog createDog(Dog dog) {
+    @Transactional
+    public Dog createDog(Dog dog) {
         return dogDao.createDog(dog);
     }
 
-    @Transactional public final boolean deleteDog(String id) {
+    @Transactional
+    public boolean deleteDog(String id) {
         return dogDao.deleteDog(id);
     }
 
